b3ec396292ccbfd452ffcd8f329ff605c0902feb
Dimple-Kanwar <dimplekanwar.dks@gmail.com>
2021-06-30 13:04:07 +0530

    added logo for ONE

--- src/templates/BillOfLading/BillOfLadingTemplate.tsx
+++ src/templates/BillOfLading/BillOfLadingTemplate.tsx
@@ -149,7 +149,7 @@ const Section1 = (document: BillOfLading): JSX.Element => {
       <div className="flex">
         <div className="w-1/2 border-black border">
           <div className="p-2 h-full flex justify-center items-center">
-            <img data-testid="logo" style={{ width: "150px" }} src="/static/images/logo-trames.png" />
+            <img data-testid="logo" style={{ width: "150px" }} src="/static/images/one-logo.png" />
           </div>
         </div>
         <div className="w-1/2">
